18b5833ab318f287d156e271d5790a6ad9e3f6f2
John Li <ozapp@users.noreply.github.com>
2015-12-01 21:32:48 +1100

    add default two break points

--- remsponsive.js
+++ remsponsive.js
@@ -1,14 +1,38 @@
 (function() {
     'use strict';
 
-    function setRootFontSize(args) {
-        // default design is for 1024px width
-        var fontSize = window.innerWidth / 1024;
+    // global namespace
+    var Remsponsive = Remsponsive || {};
+    Remsponsive.breakPoints = [];
+
+
+    Remsponsive.init = function(breakPoints) {
+        if (!Array.isArray(breakPoints)) {
+            console.log('Remsponsive init error: breakPoints should be an Array.');
+        } else {
+            this.breakPoints = breakPoints;
+        }
+    };
+
+    // default design is for 1024px width
+    Remsponsive.init([640, 1024]);
+
+    Remsponsive.setRootFontSize = function() {
+        var fontSize;
+        if (window.innerWidth < Remsponsive.breakPoints[0]) {
+            fontSize = window.innerWidth / Remsponsive.breakPoints[0];
+        } else {
+            fontSize = window.innerWidth / Remsponsive.breakPoints[1];
+        }
         document.getElementsByTagName("html")[0].style.fontSize = fontSize + "px";
-    }
-    window.addEventListener("orientationchange", function(events) {
-        setRootFontSize();
-    },true);
-    window.onload = setRootFontSize;
-    window.onresize = setRootFontSize;
+    };
+
+    window.addEventListener("orientationchange", function() {
+        Remsponsive.setRootFontSize();
+    }, true);
+    window.onload = Remsponsive.setRootFontSize;
+    window.onresize = Remsponsive.setRootFontSize;
+
+
+    //console.log(Remsponsive);
 })();
